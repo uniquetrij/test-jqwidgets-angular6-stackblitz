b881442c17069d46706aaafe46a61ce0f304d56c
Trijeet Modak <uniquetrij@gmail.com>
2018-08-15 15:46:28 +0530

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+ 
+@Component({
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
+})
+ 
+export class AppComponent {
+    values: number[] = [102, 115, 130, 137];
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,15 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+import { jqxBarGaugeComponent } from 'jqwidgets-scripts/jqwidgets-ts/angular_jqxbargauge';
+@NgModule({
+  declarations: [
+    AppComponent, jqxBarGaugeComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
